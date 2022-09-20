18d66cd7097113f1f0828f4922985b24bfadb20c
Mardonio Costa <mardonio@live.com>
2022-09-20 08:40:45 -0400

    Criado projeto principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'content1',
+    children: [{ path: '**', component: EmptyRouteComponent }],
+  },
+  {
+    path: 'content2',
+    children: [{ path: '**', component: EmptyRouteComponent }],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
